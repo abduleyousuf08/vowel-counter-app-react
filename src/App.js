21caaa36af1999c8bdfa10d501dd81242c4db8c3
AbduleYousuf <abduleyousuf08@gmail.com>
2022-12-22 21:07:30 +0300

    vowel-counter-app reactjs

--- src/App.js
+++ src/App.js
@@ -3,39 +3,38 @@ import { FcCalculator } from "react-icons/fc";
 import { useState } from "react"
 
 function App() {
-  const [input, setInput] = useState()
-  const [totally, setTotally] = useState()
-  function calculate() {
-    if (input >= 50 && input <= 300) {
-      let tip1 = input * 0.15
-      let total1 = input * 0.15 / 0.15 + input * 0.15
-      let final = (`Your bill is ${input} and your tip is ${tip1} and finally your total is ${total1}`)
-      setTotally(final)
+  const [input, setInput] = useState([])
+  const [answer, setAns] = useState("")
+  const [number, setNum] = useState()
 
-    } else {
-      let tip2 = input * 0.20
-      let total2 = input * 0.20 / 0.20 + input * 0.20
-      let final = (`Your bill is ${input} and your tip is ${tip2} and finally your total is ${total2}`)
-      setTotally(final)
-    }
+
+
+
+  function vowelsCheck() {
+    let array = (input.split(""))
+    let update = array.filter(item => item.includes("a") || item.includes("e") || item.includes("u") || item.includes("o"))
+    setNum(update.length)
+    setAns(update)
+
   }
-  return (
-    <div class="parent">
-      <div class="container">
-        <div class="child">
-          <h1 class="header">TIP CALCULATOR <FcCalculator class="icon" /> </h1>
-          <label class="label-0">Enter your bill :-
-            <input class="input-0" type="number" placeholder="Enter here" onChange={(e) => setInput(e.target.value)} />
-          </label>
-          <br />
-          <br />
-          <button class="btn" onClick={calculate}>Check</button>
-          <br />
 
-          <h2 class="result">{totally}</h2>
-        </div>
+
+
+
+  return (
+
+    <div>
+      <h1>Vowel Counter App</h1>
+
+      <input type="text" class="input" onChange={(e) => setInput(e.target.value)} /><br />
+
+      <button class="btn" onClick={vowelsCheck}>Calculate</button>
+      <p class="answer">{input} has {number} vowels:- {answer}</p>
+
+      <div class="results">
+        <center><b>Previous Searchs</b></center>
       </div>
-    </div >
+    </div>
   );
 }
 
